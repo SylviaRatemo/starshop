f4de7db8566cfb665e4cab467a668880ecea62d3
SylviaRatemo <ratemosylvia@gmail.com>
2024-09-28 01:56:24 +0300

    End of lesson twig inheritance

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,36 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
+        $starshipCount = 99;
+
+        $myShip = [
+            'name' => 'Jedi Starship',
+            'model' => 'Starship',
+            'manufacturer' => 'Jedi Order',
+            'cost_in_credits' => '100000',
+            'length' => '20',
+            'max_atmosphering_speed' => '1000',
+            'crew' => '1',
+            'passengers' => '1',
+            'cargo_capacity' => '100',
+            'consumables' => '1 month',
+            'hyperdrive_rating' => '1.0',
+            'MGLT' => '100',
+            'starship_class' => 'Starfighter',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
     }
 }
